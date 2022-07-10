8a08f4eb2e61b85477b02930f7421b8a060fdbab
Marina <m.o.ganzera@student.csn.khai.edu>
2022-07-10 15:41:48 +0300

    добавление коментарий 2.0

--- summer_practice/Header.h
+++ summer_practice/Header.h
@@ -1,9 +1,10 @@
 #ifndef HEADER_H
 #define HEADER_H
 #define _CRT_SECURE_NO_WARNINGS
+//підключення бібліотек
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-int cmp(const void* a, const void* b);
+int cmp(const void* a, const void* b); //оголошення функції
 #endif
