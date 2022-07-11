5e0edce0da19e5bae014c6f1be49ebba4488bc15
Marina <m.o.ganzera@student.csn.khai.edu>
2022-07-11 18:53:41 +0300

    тест для первой фунции

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,10 +1,15 @@
 #include "pch.h"
+#include <string.h>
+#include <ctype.h>
 #include "..\..\summer_practice\func.c"
 
 TEST(test, replace1) {
-	EXPECT_EQ(1, 1);
+	char mystring[300] = { 0 }, copystring[300]; //string declaration
+	replace1(mystring);
+	EXPECT_STREQ(mystring, "");
 }
-
+/*
 TEST(test, replace2) {
-	EXPECT_EQ(1, 1);
+	EXPECT_EQ(replace2("hello world"),"ehllo dlorw");
 }
+*/
